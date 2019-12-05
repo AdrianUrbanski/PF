6b446f4be46a6423d3a08c880383108c8afbd95a
AdrianUrbanski <adrianu161999@gmail.com>
2019-12-05 15:59:24 +0100

    change list8, task 3

--- Lista8/zad3.ml
+++ Lista8/zad3.ml
@@ -22,6 +22,7 @@ module type EDGE = sig
   type t
   type label
   type vertex
+  module V : VERTEX
 
   val equal : t -> t -> bool
   val create : vertex -> vertex -> label -> t
@@ -31,8 +32,9 @@ module type EDGE = sig
 end
 
 
-module Edge (Vertex : VERTEX) : EDGE with type vertex = Vertex.t and type label = int =
+module Edge : EDGE with type label = int and module V = Vertex and type vertex = Vertex.t =
 struct
+  module V = Vertex
   type vertex = Vertex.t
   type label = int
   type t = E of vertex*vertex*label
@@ -44,7 +46,7 @@ struct
   let into e = match e with
       E(_, v2, _) -> v2
   let equal e1 e2 = 
-    (Vertex.equal (from e1) (from e2)) && (Vertex.equal (into e1) (into e2)) && (label e1)=(label e2)
+    (V.equal (from e1) (from e2)) && (V.equal (into e1) (into e2)) && (label e1)=(label e2)
   let create v1 v2 l = E(v1,v2,l)
 end
 
@@ -54,7 +56,7 @@ module type GRAPH = sig
   module V : VERTEX
   type vertex = V.t
   module E : EDGE with type vertex = vertex
-  type edge = E.t
+  type edge = E.t 
 
   (* funkcje wyszukiwania *)
   val mem_v : t -> vertex -> bool
@@ -77,3 +79,171 @@ module type GRAPH = sig
   val fold_v : (vertex ->'a ->'a) -> t ->'a ->'a
   val fold_e : (edge ->'a ->'a) -> t ->'a ->'a
 end
+
+module Graph : GRAPH with module V = Vertex and module E = Edge =
+struct
+  module V = Vertex
+  type vertex = V.t
+  module E = Edge
+  type edge = E.t
+  type vs = VNil
+          | VCons of vertex*vs
+  type es = ENil
+          | ECons of edge*es
+  type t = G of vs*es
+
+  (* funkcje wyszukiwania *)
+  let mem_v g v = match g with G(vs, es) ->
+    let rec aux vs =
+      match vs with
+        VNil -> false
+      | VCons(hdvs, tlvs) ->
+        if V.equal hdvs v
+        then true
+        else aux tlvs
+    in
+    aux vs
+
+  let mem_e g e = match g with G(vs, es) ->
+    let rec aux es =
+      match es with
+        ENil -> false
+      | ECons(hdes, tles) ->
+        if E.equal hdes e
+        then true
+        else aux tles
+    in
+    aux es
+
+  let mem_e_v g v1 v2 = match g with G(vs, es) ->
+    let rec aux es =
+      match es with
+        ENil -> false
+      | ECons(hdes, tles) ->
+        if (V.equal (E.from hdes) v1)&&(V.equal (E.into hdes) v2)
+        then true
+        else aux tles
+    in
+    aux es
+
+  let find_e g v1 v2 = match g with G(vs, es) ->
+    let rec aux es =
+      match es with
+        ENil -> failwith "Edge not found"
+      | ECons(hdes, tles) ->
+        if (V.equal (E.from hdes) v1)&&(V.equal (E.into hdes) v2)
+        then hdes
+        else aux tles
+    in
+    aux es
+
+  let succ g v = match g with G(vs, es) ->
+    let rec aux succs es =
+      match es with
+        ENil -> succs
+      | ECons(hdes, tles) ->
+        if (V.equal (E.from hdes) v)
+        then aux ((E.into hdes)::succs) tles
+        else aux succs tles
+    in
+    aux [] es
+
+  let pred g v = match g with G(vs, es) ->
+    let rec aux preds es =
+      match es with
+        ENil -> preds
+      | ECons(hdes, tles) ->
+        if (V.equal (E.into hdes) v)
+        then aux ((E.from hdes)::preds) tles
+        else aux preds tles
+    in
+    aux [] es
+
+  let succ_e g v = match g with G(vs, es) ->
+    let rec aux succs es =
+      match es with
+        ENil -> succs
+      | ECons(hdes, tles) ->
+        if (V.equal (E.from hdes) v)
+        then aux (hdes::succs) tles
+        else aux succs tles
+    in
+    aux [] es
+
+  let pred_e g v = match g with G(vs, es) ->
+    let rec aux preds es =
+      match es with
+        ENil -> preds
+      | ECons(hdes, tles) ->
+        if (V.equal (E.into hdes) v)
+        then aux (hdes::preds) tles
+        else aux preds tles
+    in
+    aux [] es
+
+  (* funkcje modyfikacji *)
+  let empty =
+    G(VNil, ENil)
+
+  let add_e g e = match g with G(vs, es) ->
+    G(vs, ECons(e, es))
+
+  let add_v g v = match g with G(vs, es) ->
+    G(VCons(v, vs), es)
+
+  let rem_e g e = match g with G(vs, es) ->
+    let rec aux es =
+      match es with
+        ENil -> ENil
+      | ECons(hdes, tles) ->
+        if (E.equal hdes e)
+        then aux tles
+        else ECons(hdes, aux tles)
+    in
+    G(vs, aux es)
+
+  let rem_v g v = match g with G(vs, es) ->
+    let rec aux vs =
+      match vs with
+        VNil -> VNil
+      | VCons(hdvs, tlvs) ->
+        if (V.equal hdvs v)
+        then aux tlvs
+        else VCons(hdvs, aux tlvs)
+    in
+    G(aux vs, es)
+
+  (* iteratory *)
+  let fold_v f g a = match g with G(vs, es) ->
+    let rec aux vs =
+      match vs with
+        VNil -> a
+      | VCons(hdvs, tlvs) -> f hdvs (aux tlvs)
+    in
+    aux vs
+
+  let fold_e f g a = match g with G(vs, es) ->
+    let rec aux es =
+      match es with
+        ENil -> a
+      | ECons(hdes, tles) -> f hdes (aux tles)
+    in
+    aux es
+end
+
+let v1 = Vertex.create 1
+let v2 = Vertex.create 2
+let v3 = Vertex.create 3
+let e1 = Edge.create v1 v2 01
+let e2 = Edge.create v1 v3 02
+let g1 =
+let open Graph in
+(add_e (add_e (add_v (add_v (add_v empty v1) v2) v3) e1) e2);;
+
+Graph.mem_v g1 v1;;
+Graph.mem_e g1 e1;;
+Edge.equal (Graph.find_e g1 v1 v2) e1;;
+Graph.mem_e (Graph.rem_e g1 e1) e1;;
+List.map Vertex.label (Graph.succ g1 v1);;
+List.map Vertex.label (Graph.pred g1 v2);;
+List.map Edge.label (Graph.succ_e g1 v1)
--- Lista8/zad3a.ml
+++ Lista8/zad3a.ml
@@ -21,8 +21,8 @@ end
 module type EDGE = sig
   type t
   type label
-  module V: VERTEX
-  type vertex = V.t
+  type vertex
+  module V : VERTEX
 
   val equal : t -> t -> bool
   val create : vertex -> vertex -> label -> t
@@ -32,10 +32,10 @@ module type EDGE = sig
 end
 
 
-module Edge : EDGE with type label = int =
+module Edge : EDGE with type label = int and module V = Vertex and type vertex = Vertex.t =
 struct
   module V = Vertex
-  type vertex = V.t
+  type vertex = Vertex.t
   type label = int
   type t = E of vertex*vertex*label
 
@@ -56,7 +56,7 @@ module type GRAPH = sig
   module V : VERTEX
   type vertex = V.t
   module E : EDGE with type vertex = vertex
-  type edge = E.t
+  type edge = E.t 
 
   (* funkcje wyszukiwania *)
   val mem_v : t -> vertex -> bool
@@ -80,11 +80,11 @@ module type GRAPH = sig
   val fold_e : (edge ->'a ->'a) -> t ->'a ->'a
 end
 
-module type Graph : GRAPH =
+module Graph (Ver:VERTEX) (Edg:EDGE with type vertex = Ver.t) : GRAPH with module V = Ver and module E = Edg =
 struct
-  module V = Vertex
+  module V = Ver
   type vertex = V.t
-  module E = Edge
+  module E = Edg
   type edge = E.t
   type vs = VNil
           | VCons of vertex*vs
@@ -93,8 +93,8 @@ struct
   type t = G of vs*es
 
   (* funkcje wyszukiwania *)
-  let mem_v (vs, _) v =
-    let aux vs =
+  let mem_v g v = match g with G(vs, es) ->
+    let rec aux vs =
       match vs with
         VNil -> false
       | VCons(hdvs, tlvs) ->
@@ -104,8 +104,8 @@ struct
     in
     aux vs
 
-  let mem_e (_, es) e =
-    let aux es =
+  let mem_e g e = match g with G(vs, es) ->
+    let rec aux es =
       match es with
         ENil -> false
       | ECons(hdes, tles) ->
@@ -113,10 +113,10 @@ struct
         then true
         else aux tles
     in
-    aux vs
+    aux es
 
-  let mem_e_v (_, es) v1 v2 =
-    let aux es =
+  let mem_e_v g v1 v2 = match g with G(vs, es) ->
+    let rec aux es =
       match es with
         ENil -> false
       | ECons(hdes, tles) ->
@@ -124,10 +124,10 @@ struct
         then true
         else aux tles
     in
-    aux vs
+    aux es
 
-  let find_e (_, es) v1 v2 =
-    let aux es =
+  let find_e g v1 v2 = match g with G(vs, es) ->
+    let rec aux es =
       match es with
         ENil -> failwith "Edge not found"
       | ECons(hdes, tles) ->
@@ -135,47 +135,47 @@ struct
         then hdes
         else aux tles
     in
-    aux vs
+    aux es
 
-  let succ (_, es) v =
-    let aux succs es =
+  let succ g v = match g with G(vs, es) ->
+    let rec aux succs es =
       match es with
         ENil -> succs
       | ECons(hdes, tles) ->
-        if (V.eq (E.from hdes) v)
+        if (V.equal (E.from hdes) v)
         then aux ((E.into hdes)::succs) tles
         else aux succs tles
     in
     aux [] es
 
-  let pred (_, es) v =
-    let aux preds es =
+  let pred g v = match g with G(vs, es) ->
+    let rec aux preds es =
       match es with
         ENil -> preds
       | ECons(hdes, tles) ->
-        if (V.eq (E.into hdes) v)
+        if (V.equal (E.into hdes) v)
         then aux ((E.from hdes)::preds) tles
         else aux preds tles
     in
     aux [] es
 
-  let succ_e (_, es) v =
-    let aux succs es =
+  let succ_e g v = match g with G(vs, es) ->
+    let rec aux succs es =
       match es with
         ENil -> succs
       | ECons(hdes, tles) ->
-        if (V.eq (E.from hdes) v)
+        if (V.equal (E.from hdes) v)
         then aux (hdes::succs) tles
         else aux succs tles
     in
     aux [] es
 
-  let pred_e (_, es) v =
-    let aux preds es =
+  let pred_e g v = match g with G(vs, es) ->
+    let rec aux preds es =
       match es with
         ENil -> preds
       | ECons(hdes, tles) ->
-        if (V.eq (E.into hdes) v)
+        if (V.equal (E.into hdes) v)
         then aux (hdes::preds) tles
         else aux preds tles
     in
@@ -183,50 +183,111 @@ struct
 
   (* funkcje modyfikacji *)
   let empty =
-    (VNil, ENil)
+    G(VNil, ENil)
 
-  let add_e (vs, es) e =
-    (vs, ECons(e, es))
+  let add_e g e = match g with G(vs, es) ->
+    G(vs, ECons(e, es))
 
-  let add_v (vs, es) v =
-    (VCons(v, vs), es)
+  let add_v g v = match g with G(vs, es) ->
+    G(VCons(v, vs), es)
 
-  let rem_e (vs, es) e =
-    let aux es =
+  let rem_e g e = match g with G(vs, es) ->
+    let rec aux es =
       match es with
         ENil -> ENil
       | ECons(hdes, tles) ->
-        if (E.eq hdes e)
+        if (E.equal hdes e)
         then aux tles
         else ECons(hdes, aux tles)
     in
-    (vs, aux es)
+    G(vs, aux es)
 
-  let rem_v (vs, es) v =
-    let aux vs =
+  let rem_v g v = match g with G(vs, es) ->
+    let rec aux vs =
       match vs with
         VNil -> VNil
       | VCons(hdvs, tlvs) ->
-        if (V.eq hdvs v)
+        if (V.equal hdvs v)
         then aux tlvs
         else VCons(hdvs, aux tlvs)
     in
-    (aux vs, es)
+    G(aux vs, es)
 
   (* iteratory *)
-  let fold_v f (vs, es) a =
+  let fold_v f g a = match g with G(vs, es) ->
     let rec aux vs =
       match vs with
         VNil -> a
-        VCons(hdvs, tlvs) -> f hdvs (aux tlvs)
+      | VCons(hdvs, tlvs) -> f hdvs (aux tlvs)
     in
     aux vs
 
-  let fold_e f (vs, es) a =
+  let fold_e f g a = match g with G(vs, es) ->
     let rec aux es =
       match es with
         ENil -> a
-        ECons(hdes, tles) -> f hdes (aux tles)
+      | ECons(hdes, tles) -> f hdes (aux tles)
     in
     aux es
 end
+
+module GraphInt = Graph(Vertex)(Edge)
+
+let v1 = Vertex.create 1
+let v2 = Vertex.create 2
+let v3 = Vertex.create 3
+let e1 = Edge.create v1 v2 11
+let e2 = Edge.create v1 v3 12
+let g1 =
+let open GraphInt in
+(add_e (add_e (add_v (add_v (add_v empty v1) v2) v3) e1) e2);;
+
+GraphInt.mem_v g1 v1;;
+GraphInt.mem_e g1 e1;;
+Edge.equal (GraphInt.find_e g1 v1 v2) e1;;
+GraphInt.mem_e (GraphInt.rem_e g1 e1) e1;;
+List.map Vertex.label (GraphInt.succ g1 v1);;
+List.map Vertex.label (GraphInt.pred g1 v2);;
+List.map Edge.label (GraphInt.succ_e g1 v1);;
+
+
+(*
+let dfs (type a) (type b) (module G : GRAPH with type t = a and type V.t = b) g v =
+  let rec aux visited v =
+    List.fold_left aux (v::visited) (G.succ g v)
+  in
+  List. rev (aux [] v)
+   *)
+
+let dfs g v =
+  let rec aux visited v =
+    let step visited v =
+      if not(List.mem v visited)
+      then aux visited v
+      else visited
+    in
+    List.fold_left step (v::visited) (GraphInt.succ g v)
+  in
+  List. rev (aux [] v)
+
+let v4 = Vertex.create 4
+let e3 = Edge.create v2 v4 13
+let g2 =
+  let open GraphInt in
+  add_e (add_v g1 v4) e3;;
+
+List.map Vertex.label (dfs g2 v1);;
+
+let bfs g v =
+  let rec aux queue visited v =
+    match queue with
+      [] -> if(List.mem v visited)
+      then visited
+      else (v::visited)
+    | next::qtl -> if(List.mem v visited)
+      then aux qtl visited next
+      else aux (qtl@(GraphInt.succ g v)) (v::visited) next
+  in
+  List.rev (aux (GraphInt.succ g v) [] v);;
+
+List.map Vertex.label (bfs g2 v1)
