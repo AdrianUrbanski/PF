64a9eb4cee03e543085d5d454dc294f2f9f9ff84
AdrianUrbanski <adrianu161999@gmail.com>
2019-12-13 18:32:54 +0100

    add list 9, tasks 4-6

--- Lista9/zadania.hs
+++ Lista9/zadania.hs
@@ -1,3 +1,5 @@
+import Data.List (inits, tails)
+
 -- zad 1
 f :: [Integer] -> [Integer]
 f (hd:tl) =
@@ -15,31 +17,48 @@ fib :: [Integer]
 fib = 0:1:zipWith (+) fib (tail fib)
 
 -- zad 4
-revApp :: [a] -> [a] -> [a]
-revApp [] ys = ys
-revApp (x:xs) ys = revApp xs (x:ys)
-
 insert :: [a] -> a -> [[a]]
-insert perm elem = aux [] [] perm elem where
-  aux :: [[b]] -> [b] -> [b] -> b -> [[b]]
-  aux acc beg [] elem = ((revApp beg [elem]):acc)
-  aux acc beg (x:end) elem = aux ((revApp beg (elem:x:end)):acc) (x:beg) end elem
+insert xs elem = zipWith (\init -> \tail -> init++[elem]++tail) (inits xs) (tails xs)
 
 iperm :: [a] -> [[a]]
-iperm xs = aux [] xs where
+iperm xs = aux [[]] xs where
   aux :: [[a]] -> [a] -> [[a]]
   aux acc [] = acc
   aux acc (elem:rest) = aux (concat [insert perm elem | perm <- acc]) rest
 
--- sperm :: [a] -> [[a]]
+iperm' :: [a] -> [[a]]
+iperm' xs = aux [[]] xs where
+  aux :: [[a]] -> [a] -> [[a]]
+  aux acc [] = acc
+  aux acc (elem:rest) = aux (concatMap (\xs -> insert xs elem) acc) rest
+
+select :: ([a],[a]) -> [([a],[a])]
+select (xs, ys) = aux xs [] [] ys where
+  aux :: [a] -> [([a], [a])] -> [a] -> [a] -> [([a], [a])]
+  aux xs acc init [] = acc
+  aux xs acc init (elem:tail) = aux xs ((elem:xs, (reverse init)++tail):acc) (elem:init) tail
+
+sperm :: [a] -> [[a]]
+sperm xs = map fst (aux [([], xs)]) where
+  aux :: [([a],[a])] -> [([a],[a])]
+  aux xs = if (snd (head xs)) == []
+           then xs
+           else concatMap select xs
 
 -- zad 5
 
--- sublist :: [a] -> [[a]]
+sublists :: [a] -> [[a]]
+sublists xs = map reverse (aux [[]] xs) where
+  aux :: [[a]] -> [a] -> [[a]]
+  aux acc [] = acc
+  aux acc (elem:rest) = aux (acc++[elem:x | x <- acc]) rest
 
 -- zad 6
 
--- qsortBy :: (a -> a -> Bool) -> [a] -> [a]
+qsortBy :: (a -> a -> Bool) -> [a] -> [a]
+qsortBy f [] = []
+qsortBy f (x:[]) = (x:[])
+qsortBy f (hd:tl) = (qsortBy f [x | x <- tl, f hd x])++[hd]++(qsortBy f [x | x <- tl, not(f hd x)])
 
 -- zad 7
 
@@ -49,11 +68,13 @@ data Tree a = Node (Tree a) a (Tree a) | Leaf
 treeEmpty :: Ord a => Tree a
 treeEmpty = Leaf
 
-treeAdd :: Ord a => a -> Tree a -> Tree a
-treeAdd a (Leaf) = Node Leaf a Leaf
-treeAdd a (Node l v r) | a == v = (Node l v r) -- values are unique
+treeInsert :: Ord a => a -> Tree a -> Tree a
+treeInsert a (Leaf) = Node Leaf a Leaf
+treeInsert a (Node l v r) | a == v = (Node l v r) -- values are unique
                        | a < v = (Node (treeAdd a l) v r)
                        | a > v = (Node l v (treeAdd a r))
+
+treeRemove :: Ord a => a -> Tree a -> Tree a
 
 treeFromList :: Ord a => [a] -> Tree a
 treeFromList xs = foldr treeAdd treeEmpty xs
@@ -88,19 +109,19 @@ setFull = Cofin(treeEmpty)
 
 setUnion :: Ord a => Set a -> Set a -> Set a
 setUnion = Fin(t1) Fin(t2) = Fin(treeMerge t1 t2)
-setUnion = Fin(t1) Cofin(t2) = Cofin(treeIntersection t1 t2)
+setUnion = Fin(t1) Cofin(t2) = Cofin(treeSub t1 t2)
 -- s1' U s2 = (s1\s2)'
-setUnion = Cofin(t1) Fin(t2) = Cofin(treeIntersection t1 t2)
+setUnion = Cofin(t1) Fin(t2) = Cofin(treesub t2 t1)
 -- s1 U s2' = (s2\s1)'
 setUnion = Cofin(t1) Cofin(t2) = Cofin(treeIntersection t1 t2)
 -- s1' U s2' = (s2/\s1)'
 
 setIntersection :: Ord a => Set a -> Set a -> Set a
 setIntersection Fin(t1) Fin(t2) = Fin(treeIntercetion t1 t2)
-setIntersection Cofin(t1) Fin(t2) = Fin(treeIntercetion t1 t2)
+setIntersection Cofin(t1) Fin(t2) = Fin(treeSub t1 t2)
 -- s1' /\ s2 = s2\s1
-setIntersection Fin(t1) Cofin(t2) = Fin(treeIntercetion t1 t2)
+setIntersection Fin(t1) Cofin(t2) = Fin(treeSub t1 t2)
 -- s1 /\ s2' = s1\s2
-setIntersection Cofin(t1) Cofin(t2) = Fin(treeIntercetion t1 t2)
+setIntersection Cofin(t1) Cofin(t2) = Cofin(treeSum t1 t2)
 -- s1' /\ s2' = (s1Us2)'
 -}
